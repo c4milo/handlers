4648cd1fbd7cf70b1857b2919acec18f3a50289d
Camilo Aguilar <camilo.aguilar@gmail.com>
2017-01-17 15:11:13 -0500

    grpcutil: add examples to godoc

---
+++ grpcutil/example_client_test.go
@@ -0,0 +1,47 @@
+package grpcutil_test
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"log"
+
+	"github.com/c4milo/handlers/grpcutil"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func Example_client() {
+	tlsKeyPair, err := tls.LoadX509KeyPair("testdata/cert.pem", "testdata/key.pem")
+	if err != nil {
+		panic(err)
+	}
+
+	x509Cert, err := x509.ParseCertificate(tlsKeyPair.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(x509Cert)
+
+	clientCreds := credentials.NewClientTLSFromCert(certPool, "")
+	clientOpts := []grpc.DialOption{
+		grpc.WithTransportCredentials(clientCreds),
+	}
+
+	clientConn, err := grpc.Dial("localhost:8080", clientOpts...)
+	if err != nil {
+		panic(err)
+	}
+
+	defer clientConn.Close()
+
+	test := grpcutil.NewTestClient(clientConn)
+	res, err := test.Hola(context.Background(), &grpcutil.HolaRequest{})
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(res.Greeting)
+}
---
+++ grpcutil/example_server_test.go
@@ -0,0 +1,79 @@
+package grpcutil_test
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/c4milo/handlers/grpcutil"
+	"github.com/c4milo/handlers/logger"
+	"golang.org/x/net/context"
+)
+
+type Service struct{}
+
+// Hola prints greeting message
+func (s *Service) Hola(ctx context.Context, r *grpcutil.HolaRequest) (*grpcutil.HolaResponse, error) {
+	return &grpcutil.HolaResponse{Greeting: "Hola from gRPC service!"}, nil
+}
+
+func registerService(binding grpcutil.ServiceBinding) error {
+	grpcutil.RegisterTestServer(binding.GRPCServer, new(Service))
+	return grpcutil.RegisterTestHandler(context.Background(), binding.GRPCGatewayMuxer, binding.GRPCGatewayClient)
+}
+
+func Example_server() {
+	tlsKeyPair, err := tls.LoadX509KeyPair("testdata/cert.pem", "testdata/key.pem")
+	if err != nil {
+		panic(err)
+	}
+
+	mux := http.DefaultServeMux
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Hola from HTTP handler!")
+	})
+
+	handler := logger.Handler(mux)
+	options := []grpcutil.Option{
+		grpcutil.WithTLSCert(&tlsKeyPair),
+		grpcutil.WithPort("8080"),
+		grpcutil.WithServices([]grpcutil.ServiceRegisterFn{registerService}),
+	}
+	handler = grpcutil.Handler(handler, options...)
+
+	srv := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{tlsKeyPair},
+			NextProtos:   []string{"h2"},
+		},
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			if srv != nil {
+				srv.Close()
+			}
+		}
+	}()
+
+	done := make(chan bool)
+	fmt.Printf("Starting server in %q... ", srv.Addr)
+	go func() {
+		if err := srv.ListenAndServeTLS("", ""); err != nil {
+			done <- true
+			if err != http.ErrServerClosed {
+				panic(err)
+			}
+		} else {
+			done <- true
+		}
+	}()
+	fmt.Println("done")
+	<-done
+}
